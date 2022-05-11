efb0352f2a5e755e5db394aa42ea3e113b68a2f5
Amritaansh Narain <namritaansh02@gmail.com>
2022-05-11 20:30:49 +0530

    Improved Shooting to nearerst guy

--- screencapture.py
+++ screencapture.py
@@ -9,33 +9,32 @@ from realtime_detect import process_image
 def process_map(original_image):
     lower = np.array([0,0,0])
     upper = np.array([255,10,255])
+    #Values I was testing to filter out walkable area
 
     map = original_image[30:180, 0:150]
-    # map = cv2.cvtColor(map, cv2.COLOR_RGB2HSV)
-    # map = cv2.inRange(map, lower, upper)
     return map
 
-def debug_keypress(request):    
-    while(request):
-        print('down')
-        PressKey(W)
-        time.sleep(3)
-        print('up')
-        ReleaseKey(W)
-
 def process_objects(r_image, ObjectsList):
+    if np.shape(ObjectList)[0]==0:
+        return
+    pos_x, pos_y = pyautogui.position()
+    closest_object = 0
+    offset = 35
+    curr_dist = (ObjectList[0][4]-pos_x)**2 + (ObjectList[0][5]+offset-pos_y)**2
     for i in range(len(ObjectsList)):
-        offset = 35
-        pyautogui.moveTo(ObjectsList[i][4], ObjectsList[i][5]+offset)
-        image_x = r_image.shape[1] / 2
-        image_y = (r_image.shape[0] / 2) + offset
-
-        target_interval = 20
-        cv2.circle(r_image,(int(image_x),int(image_y)), 4, (255,0,255), -1)
-        if ObjectsList[i][4] < image_x+target_interval and ObjectsList[i][4] > image_x-target_interval and \
-            ObjectsList[i][5]+offset < image_y+target_interval and ObjectsList[i][5]+offset > image_y-target_interval:
-                pyautogui.click()
-        return 
+        dist = (ObjectList[i][4]-pos_x)**2 + (ObjectList[i][5]+offset-pos_y)**2
+        if dist<curr_dist:
+            closest_object = i
+            curr_dist = dist 
+    
+    pyautogui.moveTo(ObjectsList[closest_object][4], ObjectsList[closest_object][5]+offset)
+    image_x = r_image.shape[1] / 2
+    image_y = (r_image.shape[0] / 2) + offset
+    target_interval = 20
+    cv2.circle(r_image,(int(image_x),int(image_y)), 4, (255,0,255), -1)
+    if ObjectsList[closest_object][4] < image_x+target_interval and ObjectsList[closest_object][4] > image_x-target_interval and \
+        ObjectsList[closest_object][5]+offset < image_y+target_interval and ObjectsList[closest_object][5]+offset > image_y-target_interval:
+            pyautogui.click()
 
 last_time = time.time()
 bool_shoot = True
